cf077f25b090e9db3b9e78716b43e38d59b5420c
Wilmer Oidor <wilmer2035@gmail.com>
2018-10-10 21:22:43 -0500

    creatting personal info page

--- easyCake/src/pages/personal-info/personal-info.ts
+++ easyCake/src/pages/personal-info/personal-info.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PersonalInfoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Camera } from '@ionic-native/camera';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,75 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'personal-info.html',
 })
 export class PersonalInfoPage {
+  @ViewChild('fileInput') fileInput;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isReadyToSave: boolean;
+
+  item: any;
+
+  form: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  	formBuilder: FormBuilder, public camera: Camera) {
+  	this.form = formBuilder.group({
+      profilePic: [''],
+      name: ['', Validators.required],
+      about: ['']
+    });
+
+    this.form.valueChanges.subscribe((v) => {
+      this.isReadyToSave = this.form.valid;
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PersonalInfoPage');
+  }
+
+  getPicture() {
+    if (Camera['installed']()) {
+      this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 96,
+        targetHeight: 96
+      }).then((data) => {
+        this.form.patchValue({ 'profilePic': 'data:image/jpg;base64,' + data });
+      }, (err) => {
+        alert('Unable to take photo');
+      })
+    } else {
+      this.fileInput.nativeElement.click();
+    }
+  }
+
+  processWebImage(event) {
+    let reader = new FileReader();
+    reader.onload = (readerEvent) => {
+
+      let imageData = (readerEvent.target as any).result;
+      this.form.patchValue({ 'profilePic': imageData });
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  getProfileImageStyle() {
+    return 'url(' + this.form.controls['profilePic'].value + ')'
+  }
+
+  /**
+   * The user cancelled, so we dismiss without sending data back.
+   */
+  cancel() {
+    this.viewCtrl.dismiss();
+  }
+
+  /**
+   * The user is done and wants to create the item, so return it
+   * back to the presenter.
+   */
+  done() {
+    if (!this.form.valid) { return; }
+    this.viewCtrl.dismiss(this.form.value);
   }
 
 }
